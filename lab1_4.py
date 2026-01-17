d458fc3a8f176118620e8e3d4d0ad581c6bf952c
chrisjtapia2 <chrisjtapia2@gmail.com>
2026-01-17 00:43:37 +0000

    Completed Lab 1

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,11 @@
-pass
+def calculate_average(num1,num2,num3):
+  return (num1 + num2 + num3) / 3
+
+def add_tax(bill_total):
+  tax_rate = 0.10
+  tax_amount = bill_total * tax_rate
+  return bill_total + tax_amount
+
+def greet_user(name):
+  return f"Hello {name}"
+  
